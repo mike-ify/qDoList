eba32d8b1b48ae990d7df02ffa4cf62f5667fec3
mike <myccon@gmail.com>
2022-09-15 13:21:28 +0200

    Add a new column that allows managing the DONE/INCOMPLETE status.

--- src/main/java/org/example/model/ToDoList.java
+++ src/main/java/org/example/model/ToDoList.java
@@ -4,12 +4,26 @@ import lombok.Data;
 
 @Data
 public class ToDoList {
-
+    private static enum Status{
+        DONE, INCOMPLETE
+    }
     private String title;
     private String description;
+    private Status status;
 
     public ToDoList(String title, String description) {
         this.title = title;
         this.description = description;
+        setStatusIncomplete();
+    }
+
+    public ToDoList setStatusDone(){
+        status = Status.DONE;
+        return this;
+    }
+
+    public ToDoList setStatusIncomplete(){
+        status = Status.INCOMPLETE;
+        return this;
     }
 }
